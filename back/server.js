df95eeb94d3f85cdba6a8cf8b1059a2ad1680d69
Sergey Sova <lestad@bk.ru>
2015-10-09 23:26:30 +0300

    First changes to back

--- back/server.js
+++ back/server.js
@@ -1,6 +1,11 @@
 
+var express = require('express');
+require('conlog')('Internalizer').disable();
+var Internalizer = express();
+
+
 if (!process.env.PORT) {
   process.env.PORT = 5005;
 }
 
-console.log('Start API on Port:', process.env.PORT);
+'Start API on Port: %s'.log(process.env.PORT);
